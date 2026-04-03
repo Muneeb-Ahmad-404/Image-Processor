564ea48c38f76aa33e17a06c414a84436f9f2fed
Muneeb <muneebahmad2005p@gmail.com>
2026-04-03 16:39:33 +0500

    Implemented MPI + OpenMP + OpenCL accelerated hair removal pipeline with multiscale morphology and optimized inpainting

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 #include "opencv2/imgproc.hpp"
 #include "opencv2/highgui.hpp"
 #include <opencv2/photo.hpp>
+#include <opencv2/core/ocl.hpp>
 #include <vector>
 #include <filesystem>
 #include <iostream>
@@ -13,6 +14,7 @@
 using namespace cv;
 using namespace std;
 using namespace filesystem;
+using namespace ocl;
 
 bool loadingImages(const string& folderPath, vector<Mat>& imgs);
 void convertToGrayscale(const vector<Mat>& imgs, vector<Mat>& grayImgs);
@@ -20,6 +22,9 @@ void resizeImages(const vector<Mat>& grayImgs, vector<Mat>& resizedImgs);
 void hairRemoval(const vector<string>& imagePaths, const string& outFolder, int rank, int size);
 
 int main(int argc, char** argv){
+    cv::ocl::setUseOpenCL(true);
+    cout << "OpenCL enabled: " << cv::ocl::useOpenCL() << endl;
+
     MPI_Init(&argc, &argv);
 
     int rank, size;
@@ -124,54 +129,72 @@ void hairRemoval(const vector<string>& imagePaths, const string& outFolder, int 
     int totalProcessed = 0;
     auto totalStart = chrono::high_resolution_clock::now();
 
-    // Split work across MPI ranks
     vector<string> localImages;
     for(size_t i = 0; i < imagePaths.size(); i++){
-        if(i % size == rank) // simple round-robin distribution
+        if(i % size == rank)
             localImages.push_back(imagePaths[i]);
     }
 
     #pragma omp parallel for schedule(dynamic)
     for(size_t i = 0; i < localImages.size(); i++){
         auto start = chrono::high_resolution_clock::now();
-        string imgPath = localImages[i];
-        Mat orig = imread(imgPath, IMREAD_COLOR);
-        if(orig.empty()) continue;
 
-        // Downscale for faster inpainting
+        string imgPath = localImages[i];
+        Mat origCPU = imread(imgPath, IMREAD_COLOR);
+        if(origCPU.empty()) continue;
+
+        // Move to GPU (UMat)
+        UMat orig;
+        origCPU.copyTo(orig);
+
+        // Downscale
         double scaleFactor = 800.0 / max(orig.cols, orig.rows);
-        Mat smallOrig, smallGray;
+        UMat smallOrig, smallGray;
         resize(orig, smallOrig, Size(), scaleFactor, scaleFactor, INTER_AREA);
         cvtColor(smallOrig, smallGray, COLOR_BGR2GRAY);
 
-        // Multiscale Blackhat
-        Mat accumMask = Mat::zeros(smallGray.size(), CV_8UC1);
+        // Multiscale mask (GPU)
+        UMat accumMask = UMat::zeros(smallGray.size(), CV_8UC1);
         vector<int> kernelSizes = {15, 25, 35};
 
         for(auto kSize : kernelSizes){
-            Mat khat, msk;
+            UMat khat, msk;
             Mat kernel = getStructuringElement(MORPH_ELLIPSE, Size(kSize, kSize));
+
             morphologyEx(smallGray, khat, MORPH_BLACKHAT, kernel);
             normalize(khat, khat, 0, 255, NORM_MINMAX);
 
             double minVal, maxVal;
             minMaxLoc(khat, &minVal, &maxVal);
             double threshVal = minVal + 0.15 * (maxVal - minVal);
-            threshold(khat, msk, threshVal, 255, THRESH_BINARY);
 
+            threshold(khat, msk, threshVal, 255, THRESH_BINARY);
             dilate(msk, msk, getStructuringElement(MORPH_ELLIPSE, Size(3,3)));
+
             bitwise_or(accumMask, msk, accumMask);
         }
 
-        Mat final;
-        int nonZero = countNonZero(accumMask);
-        if(nonZero == 0){
-            final = orig.clone();
+        UMat finalUMat;
+
+        if(countNonZero(accumMask) == 0){
+            orig.copyTo(finalUMat);
         } else {
-            Mat finalSmall;
-            inpaint(smallOrig, accumMask, finalSmall, 3.0, INPAINT_TELEA);
-            resize(finalSmall, final, orig.size(), 0, 0, INTER_CUBIC);
+            // inpaint still CPU-heavy → unavoidable
+            Mat smallOrigCPU, maskCPU, finalSmallCPU;
+            smallOrig.copyTo(smallOrigCPU);
+            accumMask.copyTo(maskCPU);
+
+            inpaint(smallOrigCPU, maskCPU, finalSmallCPU, 3.0, INPAINT_TELEA);
+
+            UMat finalSmall;
+            finalSmallCPU.copyTo(finalSmall);
+
+            resize(finalSmall, finalUMat, orig.size(), 0, 0, INTER_CUBIC);
         }
+
+        // Back to CPU for saving
+        Mat final;
+        finalUMat.copyTo(final);
 
         string outPath = outFolder + "/hair_removed_" + path(imgPath).filename().string();
         imwrite(outPath, final);
@@ -183,13 +206,14 @@ void hairRemoval(const vector<string>& imagePaths, const string& outFolder, int 
         {
             totalProcessed++;
             cout << "[Rank " << rank << "] Processed: " << imgPath
-                 << " in " << elapsed.count() << " sec, saved as: " << outPath << endl;
+                 << " in " << elapsed.count() << " sec" << endl;
         }
     }
 
     auto totalEnd = chrono::high_resolution_clock::now();
     chrono::duration<double> totalElapsed = totalEnd - totalStart;
+
     #pragma omp critical
-    cout << "[Rank " << rank << "] Total images processed: " << totalProcessed
-         << " in " << totalElapsed.count() << " sec" << endl;
+    cout << "[Rank " << rank << "] Total images processed: "
+         << totalProcessed << " in " << totalElapsed.count() << " sec" << endl;
 }
